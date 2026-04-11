b3f0807bf92a1ec7712b53a045d793542cffd2cc
Adis Tabakovic <adistabakovic619@yahoo.com>
2026-04-10 17:25:01 -0700

    Added new codes for grappling

--- addons/proto_controller/proto_controller.gd
+++ addons/proto_controller/proto_controller.gd
@@ -4,6 +4,7 @@
 # Happy prototyping!
 
 extends CharacterBody3D
+class_name ProtoPlayer
 
 ## Can we move around?
 @export var can_move : bool = true
@@ -28,6 +29,8 @@ extends CharacterBody3D
 ## How fast do we freefly?
 @export var freefly_speed : float = 25.0
 
+@export var swing_speed: float = 50.0
+
 @export_group("Input Actions")
 ## Name of Input Action to move Left.
 @export var input_left : String = "ui_left"
@@ -48,6 +51,9 @@ var mouse_captured : bool = false
 var look_rotation : Vector2
 var move_speed : float = 0.0
 var freeflying : bool = false
+
+var is_swinging:bool = false
+var current_hook_point: Hook_Point
 
 ## IMPORTANT REFERENCES
 @onready var head: Node3D = $Head
@@ -77,47 +83,13 @@ func _unhandled_input(event: InputEvent) -> void:
 			disable_freefly()
 
 func _physics_process(delta: float) -> void:
-	# If freeflying, handle freefly and nothing else
-	if can_freefly and freeflying:
-		var input_dir := Input.get_vector(input_left, input_right, input_forward, input_back)
-		var motion := (head.global_basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
-		motion *= freefly_speed * delta
-		move_and_collide(motion)
-		return
-	
-	# Apply gravity to velocity
-	if has_gravity:
-		if not is_on_floor():
-			velocity += get_gravity() * delta
-
-	# Apply jumping
-	if can_jump:
-		if Input.is_action_just_pressed(input_jump) and is_on_floor():
-			velocity.y = jump_velocity
-
-	# Modify speed based on sprinting
-	if can_sprint and Input.is_action_pressed(input_sprint):
-			move_speed = sprint_speed
-	else:
-		move_speed = base_speed
-
-	# Apply desired movement to velocity
-	if can_move:
-		var input_dir := Input.get_vector(input_left, input_right, input_forward, input_back)
-		var move_dir := (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
-		if move_dir:
-			velocity.x = move_dir.x * move_speed
-			velocity.z = move_dir.z * move_speed
-		else:
-			velocity.x = move_toward(velocity.x, 0, move_speed)
-			velocity.z = move_toward(velocity.z, 0, move_speed)
-	else:
-		velocity.x = 0
-		velocity.y = 0
-	
-	# Use velocity to actually move
-	move_and_slide()
-
+	if Input.is_action_just_pressed("Action 2") and current_hook_point:
+		is_swinging = true
+		
+	if is_swinging:
+		grap_swing(delta)
+	else : 
+		movement(delta)
 
 ## Rotate us to look around.
 ## Base of controller rotates around y (left/right). Head rotates around x (up/down).
@@ -176,3 +148,52 @@ func check_input_mappings():
 	if can_freefly and not InputMap.has_action(input_freefly):
 		push_error("Freefly disabled. No InputAction found for input_freefly: " + input_freefly)
 		can_freefly = false
+
+func grap_swing(delta:float):
+	var onGrapPoint = current_hook_point.position.distance_to(global_position) < 1
+	
+	var direction = abs(transform.basis.z) * global_position.direction_to(current_hook_point.position)
+	velocity += direction * swing_speed * delta
+func movement(delta:float):
+		# If freeflying, handle freefly and nothing else
+	if can_freefly and freeflying:
+		var input_dir := Input.get_vector(input_left, input_right, input_forward, input_back)
+		var motion := (head.global_basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
+		motion *= freefly_speed * delta
+		move_and_collide(motion)
+		return
+	
+	# Apply gravity to velocity
+	if has_gravity:
+		if not is_on_floor():
+			velocity += get_gravity() * delta
+
+	# Apply jumping
+	if can_jump:
+		if Input.is_action_just_pressed(input_jump) and is_on_floor():
+			velocity.y = jump_velocity
+
+	# Modify speed based on sprinting
+	if can_sprint and Input.is_action_pressed(input_sprint):
+			move_speed = sprint_speed
+	else:
+		move_speed = base_speed
+
+	# Apply desired movement to velocity
+	if can_move:
+		var input_dir := Input.get_vector(input_left, input_right, input_forward, input_back)
+		var move_dir := (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
+		if move_dir:
+			velocity.x = move_dir.x * move_speed
+			velocity.z = move_dir.z * move_speed
+		else:
+			velocity.x = move_toward(velocity.x, 0, move_speed)
+			velocity.z = move_toward(velocity.z, 0, move_speed)
+	else:
+		velocity.x = 0
+		velocity.y = 0
+	
+	
+	
+	# Use velocity to actually move
+	move_and_slide()
--- assets/Scripts Folder/hook_point.gd
+++ assets/Scripts Folder/hook_point.gd
@@ -1,5 +1,5 @@
 extends Area3D
-
+class_name Hook_Point
 @onready var sparkle_system: Node3D = $"Sparkle system"
 
 
@@ -7,7 +7,8 @@ extends Area3D
 # The function that singals a player has entered it's operating range 
 func _on_body_entered(body: Node3D) -> void:
 	sparkle_system.visible = true
-
+	if body is ProtoPlayer: 
+		body.current_hook_point = self
 
 func _on_body_exited(body: Node3D) -> void:
 	sparkle_system.visible = false
